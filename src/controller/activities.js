e2b22b3738a96fe9ff9063b2f4247c34add3de76
Alexis <muroa1@uci.edu>
2021-03-01 14:30:08 -0800

    Added ranking elements to display more relevant recommendations

--- src/controller/activities.js
+++ src/controller/activities.js
@@ -4,47 +4,65 @@ const activites = [
   {
     name: 'Walking',
     calories: activityCalories(120, 149, 178),
-    gym: false
+    gym: false,
+    intensity: 1,
+    time: 17
   },
   {
     name: 'Running',
     calories: activityCalories(240, 298, 355),
-    gym: false
+    gym: false,
+    intensity: 2,
+    time: 16
   },
   {
     name: 'Bicycling',
     calories: activityCalories(240, 298, 355),
-    gym: false
+    gym: false,
+    intensity: 2,
+    time: 15
   },
   {
     name: 'Yoga',
     calories: activityCalories(120, 149, 178),
-    gym: false
+    gym: false,
+    intensity: 1,
+    time: 19
   },
   {
     name: 'Jump Rope',
     calories: activityCalories(300, 372, 444),
-    gym: false
+    gym: false,
+    intensity: 3,
+    time: 19
   },
   {
     name: 'Weight Lifting',
     calories: activityCalories(90, 112, 133),
-    gym: true
+    gym: true,
+    intensity: 2,
+    time: 12
   },
   {
     name: 'Stationary Bike',
     calories: activityCalories(210, 260, 311),
-    gym: true
+    gym: true,
+    intensity: 2,
+    time: 12
   },
   {
     name: 'Swimming',
     calories: activityCalories(180, 223, 266),
-    gym: true
+    gym: true,
+    intensity: 3,
+    time: 12
   },
   {
     name: 'Stair Step Machine',
     calories: activityCalories(180, 223, 266),
-    gym: true
+    gym: true,
+    intensity: 1,
+    time: 12
   }
 ];
 
--- src/controller/engine.js
+++ src/controller/engine.js
@@ -1,14 +1,47 @@
 import activites from "./activities";
 import { getWeight } from "./GoogleFit";
+import moment from "moment";
 
 const getRecommendations = async() => {
   const weight = await getWeight();
-  const recommendations = activites.map((activity, index) => {
-    activity.id = index.toString();
-    activity.calories = Math.round(activity.calories * weight);
-    return activity;
-  });
-  recommendations.sort((a, b) => a.calories < b.calories);
+  var time = moment().get('hour');
+  console.log("TIME: "+ time);
+  var gymAccess = false;
+  var desiredIntensity = 3;
+  const recommendations = activites.map((x,index) => {
+      x.id = index.toString();
+      x.calories = Math.round(x.calories * weight);
+      x.score = x.calories;
+      if(time < x.time)
+      {
+        x.score += 200;
+        if(gymAccess && x.gym)
+        {
+            score += 300;
+        }
+      }
+      else
+      {
+        x.score -=100;
+      }
+
+      if(!gymAccess && x.gym)
+      {
+        x.score -= 300;
+      }
+
+      if(desiredIntensity == x.intensity)
+      {
+        x.score += 250;
+      }
+      else
+      {
+        x.score -= Math.abs(x.intensity - desiredIntensity)*50;
+      }
+      console.log(x.score);
+      return x;
+    });
+    recommendations.sort((a, b) => b.score - a.score);
   return recommendations;
 }
 
